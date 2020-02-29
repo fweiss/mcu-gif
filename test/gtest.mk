4c4e627b49ce61ba4740d8a424cfdfb5efd2ef5a
Frank E Weiss <feweiss@gmail.com>
2020-02-29 09:32:21 -0800

    Clean up, move dependencies

--- test/gtest.mk
+++ test/gtest.mk
@@ -1,9 +1,9 @@
 # create the gtest and gmock libraries
-$(OBJDIR)/gtest-all.o:
-	$(CXX) -c -o $@ $< $(CFLAGS) $(CXXFLAGS) $(CPPFLAGS) -I$(GTEST_HOME)/googletest/include -I$(GTEST_HOME)/googletest -c $(GTEST_HOME)/googletest/src/gtest-all.cc
+$(OBJDIR)/gtest-all.o: $(GTEST_HOME)/googletest/src/gtest-all.cc
+	$(CXX) -c -o $@ $< $(CFLAGS) $(CXXFLAGS) $(CPPFLAGS) -I$(GTEST_HOME)/googletest/include -I$(GTEST_HOME)/googletest
 
-$(OBJDIR)/gtest_main.o:
-	$(CXX) -c -o $@ $< $(CFLAGS) $(CXXFLAGS) $(CPPFLAGS) -I$(GTEST_HOME)/googletest/include -c $(GTEST_HOME)/googletest/src/gtest_main.cc
+$(OBJDIR)/gtest_main.o: $(GTEST_HOME)/googletest/src/gtest_main.cc
+	$(CXX) -c -o $@ $< $(CFLAGS) $(CXXFLAGS) $(CPPFLAGS) -I$(GTEST_HOME)/googletest/include
 
 $(OBJDIR)/libgtest.a: $(OBJDIR)/gtest-all.o
 	ar -rv $@ $<
