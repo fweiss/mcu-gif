455155d98fe37799ab461a332c0ce5bd4c43f030
Frank E Weiss <feweiss@gmail.com>
2020-03-01 08:45:15 -0800

    Use $(AR)

--- test/gtest.mk
+++ test/gtest.mk
@@ -6,10 +6,10 @@ $(OBJDIR)/gtest_main.o: $(GTEST_HOME)/googletest/src/gtest_main.cc
 	$(CXX) -c -o $@ $< $(CFLAGS) $(CXXFLAGS) $(CPPFLAGS) -I$(GTEST_HOME)/googletest/include
 
 $(OBJDIR)/libgtest.a: $(OBJDIR)/gtest-all.o
-	ar -rv $@ $<
+	$(AR) -rv $@ $<
 
 $(OBJDIR)/gmock-all.o: $(GTEST_HOME)/googlemock/src/gmock-all.cc
 	$(CXX) -c -o $@ $< $(CFLAGS) $(CXXFLAGS) $(CPPFLAGS) -I$(GTEST_HOME)/googlemock/include -I$(GTEST_HOME)/googlemock -I$(GTEST_HOME)/googletest/include
 
 $(OBJDIR)/libgmock.a: $(OBJDIR)/gmock-all.o
-	ar -rv $@ $<
+	$(AR) -rv $@ $<
