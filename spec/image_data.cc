4bf1e2b078121abb9017f3bf80f416bf20be1335
Frank E Weiss <feweiss@gmail.com>
2020-06-13 20:08:11 -0700

    Expand the decoder with some more faking

--- spec/image_data.cc
+++ spec/image_data.cc
@@ -20,17 +20,27 @@ namespace simple {
 auto image_data_spec =
 describe("image data", [] {
     uint8_t minimumCodeSize = 2;
-    // 1 byte of #4, $5, EOB
+    // 1 byte of #4, $5, 1 byte EOB
     uint8_t input[8] = { 0x01, 0x2C, 0x00 };
     uint8_t output[1024]; // fixme max output length?
     gd_image_data_block_decode_t blockDecode;
+    FFILE(input);
+    blockDecode.read = f_read;
 
-    describe("smallest block", [&] {
+    describe("zero block", [&] {
         uint16_t outputLength = gd_image_data_block_decode(&blockDecode, output);
         it("outputLength", [outputLength] {
-            expect(outputLength).to(be == 2);
+            expect(outputLength).to(be == 0);
         });
+    });
 
+    describe("one block", [&] {
+        uint8_t input[] = { 0x02, 0x4C, 0x01, 0x00 };
+        FFILE(input);
+        uint16_t outputLength = gd_image_data_block_decode(&blockDecode, output);
+        it("outputLength", [outputLength] {
+            expect(outputLength).to(be == 1);
+        });
     });
 });
 
