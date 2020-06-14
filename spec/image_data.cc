1110b28594f3c953e809acca138ff9b08e5ec1a8
Frank E Weiss <feweiss@gmail.com>
2020-06-14 09:50:39 -0700

    Refactor: extract common fixture

--- spec/image_data.cc
+++ spec/image_data.cc
@@ -17,17 +17,23 @@ using ccspec::matchers::be_truthy;
 
 namespace simple {
 
+const size_t outputSize = 1024;  // fixme max output length?
+static uint8_t output[outputSize];
+
 auto image_data_spec =
 describe("image data", [] {
-    uint8_t minimumCodeSize = 2;
-    // 1 byte of #4, $5, 1 byte EOB
-    uint8_t input[8] = { 0x01, 0x2C, 0x00 };
-    uint8_t output[1024]; // fixme max output length?
+//    uint8_t minimumCodeSize = 2;
     gd_image_data_block_decode_t blockDecode;
-    FFILE(input);
     blockDecode.read = f_read;
 
+    before("each", [] {
+        memset(output, 0, outputSize);
+    });
+
     describe("zero block", [&] {
+        // 1 byte of #4, $5, 1 byte EOB
+        uint8_t input[8] = { 0x01, 0x2C, 0x00 };
+        FFILE(input);
         uint16_t outputLength = gd_image_data_block_decode(&blockDecode, output);
         it("outputLength", [outputLength] {
             expect(outputLength).to(be == 0);
