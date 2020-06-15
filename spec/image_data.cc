cf531c1d44d13b57a1df1719495f41c9b9b6ee94
Frank E Weiss <feweiss@gmail.com>
2020-06-15 09:11:37 -0700

    Use before each

--- spec/image_data.cc
+++ spec/image_data.cc
@@ -25,25 +25,28 @@ describe("image data", [] {
 //    uint8_t minimumCodeSize = 2;
     static gd_image_data_block_decode_t blockDecode;
     blockDecode.read = f_read;
+    static uint16_t outputLength;
 
     before("each", [] {
-        memset(output, 0, outputSize); // fixme 16 but
+        memset(output, 0, outputSize); // fixme 16 bit
     });
 
     describe("one sub block", [&] {
         describe("zero codes", [&] {
-            // 1 byte of #4, $5, 1 byte EOB
-            uint8_t input[8] = { 0x01, 0x2C, 0x00 };
-            FFILE(input);
-            uint16_t outputLength = gd_image_data_block_decode(&blockDecode, output);
-            it("output length", [outputLength] {
+            before("each", [&] {
+                // 1 byte of #4, $5, 1 byte EOB
+                static uint8_t input[8] = { 0x01, 0x2C, 0x00 };
+                FFILE(input);
+                outputLength = gd_image_data_block_decode(&blockDecode, output);
+            });
+            it("output length", [&] {
                 expect(outputLength).to(be == 0);
             });
         });
 
         describe("one code", [&] {
-            static uint16_t outputLength;
             before("each", [&] {
+                // #4 #1 #5 EOB
                 static uint8_t input[] = { 0x02, 0x4C, 0x01, 0x00 };
                 FFILE(input);
                 outputLength = gd_image_data_block_decode(&blockDecode, output);
