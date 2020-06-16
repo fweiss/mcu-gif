4f9265e3a377c7f2dadcf04769f1527e1f1e60b1
Frank E Weiss <feweiss@gmail.com>
2020-06-16 12:40:57 -0700

    WIP pack DSL

--- spec/helpers.cc
+++ spec/helpers.cc
@@ -5,10 +5,17 @@ Pack::Pack() : packed() {
 }
 
 Pack& Pack::operator+(uint16_t element) {
+    printf("+ %d\n", element);
     if (packed.size() == 0) {
         packed.push_back(0x04);
     } else {
         packed[0] |= 0x28;
     }
+    return *this;
+}
+
+Pack& Pack::operator+(Shift bits) {
+    printf("<< %d\n", bits.value);
+    this->bits = bits.value;
     return *this;
 }
