399a7f3263843f5e188ed320e38b4a753c73a8e5
Frank E Weiss <feweiss@gmail.com>
2020-06-16 10:40:15 -0700

    Better test comprehension

--- spec/helpers_spec.cc
+++ spec/helpers_spec.cc
@@ -15,14 +15,26 @@ using ccspec::matchers::be_truthy;
 namespace simple {
 
 auto helpers_spec =
-describe("helpers unpack", [] {
+describe("helpers pack", [] {
+    static Pack p;
+    static std::vector<uint8_t> unpacked;
 
-    it("does", [] {
-        Pack p;
-        std::vector<uint8_t> unpacked = p + 4 + 5;
-        expect(unpacked.size()).to(eq(1));
-        expect((uint16_t)unpacked[0]).to(eq(0x2C));
+    describe("4 + 5", [&] {
+
+        before("each", [&] {
+            p.reset();
+            unpacked = p + 4 + 5;
+        });
+
+        it("size", [] {
+            expect(unpacked.size()).to(eq(1));
+        });
+
+        it("[0]", [] {
+                expect((uint16_t)unpacked[0]).to(eq(0x2C));
+        });
     });
+
 });
 
 } // namespace
