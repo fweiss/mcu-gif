d34416464c4c93db4c680ebf962d2bdf62391056
Frank E Weiss <feweiss@gmail.com>
2020-06-19 17:47:46 -0700

    Refactor: extract decompress code stream as stub for now

--- spec/gd.h
+++ spec/gd.h
@@ -21,9 +21,11 @@ typedef struct {
 typedef struct {
     uint16_t *output;
     uint16_t outputLength;
+    uint8_t compressStatus;
 } gd_image_block_t;
 
 void gd_open(gd_info_t *info);
 void gd_decode(gd_decode_t *decode);
 uint16_t gd_image_sub_block_decode(gd_image_data_block_decode_t *blockDecode, uint16_t *output);
 void gd_image_subblock_decode(gd_image_block_t *block, uint8_t *subblock, uint8_t count);
+void gd_image_decompress_code(gd_image_block_t *block, uint16_t code);
