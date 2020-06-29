869b069189ce7cb9416515ea9f7ffe57c0dc14a5
Frank E Weiss <feweiss@gmail.com>
2020-06-28 19:31:34 -0700

    WIP warming up on the expand state spec

--- spec/expand_state_spec.cc
+++ spec/expand_state_spec.cc
@@ -9,23 +9,83 @@ using ccspec::core::it;
 using ccspec::expect;
 using ccspec::matchers::eq;
 
+#include <string>
+
 #include "gd_internal.h"
 
 namespace simple {
 
 const size_t outputSize = 1024;  // fixme max output size for a sub block?
 
-typedef std::vector<uint8_t> code_stream_t;
-
 auto expand_state_spec =
 describe("expand state", [] {
+
+    static gd_expand_codes_t expand;
+    static uint16_t output[outputSize];
+
+    before("each", [] {
+        memset(output, 0, sizeof(output));
+        expand.codeSize = 3;
+        expand.output = output;
+        expand.outputLength = 0;
+
+    });
+
     describe("initial", [&] {
+
         it("code 4 clears", [&] {
-            gd_expand_codes_t image;
-            image.string_table.length = 10;
-            gd_image_expand_code(&image, 4);
-            expect(image.string_table.length).to(eq(6));
+            expand.string_table.length = 10;
+            gd_image_expand_code(&expand, 4);
+            expect(expand.string_table.length).to(eq(6));
         });
+    });
+    describe("first", [&] {
+        before("each", [] {
+                gd_image_expand_code(&expand, 4);
+        });
+        describe("found code", [&] {
+            before("each", [&] {
+                gd_image_expand_code(&expand, 2);
+            });
+            it("prior code", [] {
+                expect(expand.string.length).to(eq(1));
+                expect(expand.string.value[0]).to(eq(2));
+            });
+        });
+// behavior unspecified
+        describe("not found code", [] {
+//            before("each", [&] {
+//                gd_image_expand_code(&expand, 6);
+//            });
+//            it("prior code", [] {
+//                expect(expand.string.length).to(eq(1));
+//            });
+        });
+    });
+    describe("second", [] {
+            before("each", [] {
+                gd_image_expand_code(&expand, 4);
+                gd_image_expand_code(&expand, 2);
+                gd_image_expand_code(&expand, 6);
+            });
+            it("prior code", [] {
+                expect(expand.string.length).to(eq(2));
+                expect(expand.string.value[0]).to(eq(2));
+                expect(expand.string.value[1]).to(eq(2));
+            });
+    });
+    describe("third", [] {
+            before("each", [] {
+                gd_image_expand_code(&expand, 4);
+                gd_image_expand_code(&expand, 3);
+                gd_image_expand_code(&expand, 6);
+                gd_image_expand_code(&expand, 6);
+            });
+            it("prior code", [] {
+                expect(expand.string.length).to(eq(2));
+                expect(expand.string.value[0]).to(eq(2));
+                expect(expand.string.value[1]).to(eq(2));
+            });
     });
 });
 
