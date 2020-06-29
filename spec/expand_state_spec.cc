3513b8a2ab8c0938b1fa5f592c38cd8b9151469e
Frank E Weiss <feweiss@gmail.com>
2020-06-28 21:39:34 -0700

    Refining the expand state spec

--- spec/expand_state_spec.cc
+++ spec/expand_state_spec.cc
@@ -40,54 +40,69 @@ describe("expand state", [] {
         });
     });
     describe("scenario", [] {
-            describe("first", [&] {
+        describe("prior string ()", [] {
+            describe("found (2)", []  {
                 before("each", [] {
-                        gd_image_expand_code(&expand, 4);
+                    gd_image_expand_code(&expand, 4);
+                    expand.string.length = 0;
+                    gd_image_expand_code(&expand, 2);
                 });
-                describe("found code", [&] {
-                    before("each", [&] {
-                        gd_image_expand_code(&expand, 2);
-                    });
-                    it("prior code", [] {
-                        expect(expand.string.length).to(eq(1));
-                        expect(expand.string.value[0]).to(eq(2));
-                    });
+                it("prior length", [] {
+                     expect(expand.string.length).to(eq(1));
                 });
-        // behavior unspecified
-                describe("not found code", [] {
-        //            before("each", [&] {
-        //                gd_image_expand_code(&expand, 6);
-        //            });
-        //            it("prior code", [] {
-        //                expect(expand.string.length).to(eq(1));
-        //            });
+                it("prior value[0]", [] {
+                     expect(expand.string.value[0]).to(eq(2));
                 });
             });
-            describe("second", [] {
-                    before("each", [] {
-                        gd_image_expand_code(&expand, 4);
-                        gd_image_expand_code(&expand, 2);
-                        gd_image_expand_code(&expand, 6);
-                    });
-                    it("prior code", [] {
-                        expect(expand.string.length).to(eq(2));
-                        expect(expand.string.value[0]).to(eq(2));
-                        expect(expand.string.value[1]).to(eq(2));
-                    });
+        });
+        describe("first", [&] {
+            before("each", [] {
+                    gd_image_expand_code(&expand, 4);
             });
-            describe("third", [] {
-                    before("each", [] {
-                        gd_image_expand_code(&expand, 4);
-                        gd_image_expand_code(&expand, 3);
-                        gd_image_expand_code(&expand, 6);
-                        gd_image_expand_code(&expand, 6);
-                    });
-                    it("prior code", [] {
-                        expect(expand.string.length).to(eq(2));
-                        expect(expand.string.value[0]).to(eq(2));
-                        expect(expand.string.value[1]).to(eq(2));
-                    });
+            describe("found code", [&] {
+                before("each", [&] {
+                    gd_image_expand_code(&expand, 2);
+                });
+                it("prior code", [] {
+                    expect(expand.string.length).to(eq(1));
+                    expect(expand.string.value[0]).to(eq(2));
+                });
             });
+    // behavior unspecified
+            describe("not found code", [] {
+    //            before("each", [&] {
+    //                gd_image_expand_code(&expand, 6);
+    //            });
+    //            it("prior code", [] {
+    //                expect(expand.string.length).to(eq(1));
+    //            });
+            });
+        });
+        describe("second", [] {
+            before("each", [] {
+                gd_image_expand_code(&expand, 4);
+                gd_image_expand_code(&expand, 2);
+                gd_image_expand_code(&expand, 6);
+            });
+            it("prior code", [] {
+                expect(expand.string.length).to(eq(2));
+                expect(expand.string.value[0]).to(eq(2));
+                expect(expand.string.value[1]).to(eq(2));
+            });
+        });
+        describe("third", [] {
+            before("each", [] {
+                gd_image_expand_code(&expand, 4);
+                gd_image_expand_code(&expand, 3);
+                gd_image_expand_code(&expand, 6);
+                gd_image_expand_code(&expand, 6);
+            });
+            it("prior code", [] {
+                expect(expand.string.length).to(eq(2));
+                expect(expand.string.value[0]).to(eq(3));
+                expect(expand.string.value[1]).to(eq(3));
+            });
+        });
     });
 
 });
