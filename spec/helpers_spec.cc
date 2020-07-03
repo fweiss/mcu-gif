113b6fe6e6e86fcc01d8454e2618f86fdffe8661
Frank E Weiss <feweiss@gmail.com>
2020-07-03 09:51:45 -0700

    Postfactor: More spec comprehension

--- spec/helpers_spec.cc
+++ spec/helpers_spec.cc
@@ -30,106 +30,101 @@ static std::string dump(std::vector<uint8_t> p) {
 namespace simple {
 
 auto helpers_spec =
-describe("helpers pack", [] {
-    static Pack p;
-    static std::vector<uint8_t> packed;
+describe("helpers", [] {
 
-    before("each", [&] {
-        p.reset();
-    });
-
-    describe("4 + 5", [&] {
+    describe("pack", [] {
+        static Pack p;
+        static std::vector<uint8_t> packed;
 
         before("each", [&] {
-            packed = p + 4 + 5;
+            p.reset();
         });
 
-        it("size", [] {
-            expect(packed.size()).to(eq(1));
+        describe("4 + 5", [&] {
+
+            before("each", [&] {
+                packed = p + 4 + 5;
+            });
+
+            it("size 1", [] {
+                expect(packed.size()).to(eq(1));
+            });
+
+            it("[0]", [] {
+                expect((uint16_t)packed[0]).to(eq(0x2C));
+            });
         });
 
-        it("[0]", [] {
-            expect((uint16_t)packed[0]).to(eq(0x2C));
+        // 101 001 100 = 1 0100 1100
+        describe("4 + 1 + 5", [] {
+
+            before("each", [] {
+                packed = p + 4 + 1 + 5;
+            });
+
+            it("size 2", [] {
+                expect(packed.size()).to(eq(2));
+            });
+
+            it("output", [&] {
+                expect(dump(packed)).to(eq("4C,01,"));
+            });
+        });
+
+        describe("4 + 0 + 1 + 5", [&] {
+            before("each", [&] { packed = p + 4 + 0 + 1 + 5; });
+            it ("length 2", [&] { expect(packed.size()).to(eq(2)); });
+            it ("output", [&] { expect(dump(packed)).to(eq("44,0A,")); });
+        });
+
+        // 101 110 001 100 = 1011 1000 1100
+        describe("4 + 1 + 6 + 5", [&] {
+
+            before("each", [&] {
+                packed = p + 4 + 1 + 6 + 5;
+            });
+
+            it("length 2", [&] {
+                expect(packed.size()).to(eq(2));
+            });
+
+            it("output", [&] {
+                expect(dump(packed)).to(eq("8C,0B,"));
+            });
+        });
+
+        // 101 110 110 001 100 = 101 1101 1000 1100
+        describe("4 + 1 + 6 + 6 + 5", [&] {
+
+            before("each", [&] {
+                packed = p + 4 + 1 + 6 + 6 + 5;
+            });
+
+            it("output", [&] {
+                expect(dump(packed)).to(eq("8C,5D,"));
+            });
+        });
+
+        // 101 010 110 110 001 100 = 10 1010 1101 1000 1100
+        describe("4 + 1 + 6 + 6 + 2 + 5", [&] {
+
+            before("each", [&] { packed = p + 4 + 1 + 6 + 6 + 2 + 5; });
+
+            it("output", [&] { expect(dump(packed)).to(eq("8C,AD,02,")); });
+        });
+
+        // 0101 1001 0010 0110 110 001 100 = 0 1011 0010 0100 1101 1000 1100
+        describe("4, 1, 6, Shift(4) 6, 2, 9 (4 code bits)", [] {
+
+            before("each", [] {
+                packed = p + 4 + 1 + 6 + Shift(4) + 6  + 2 + 9 + 5;
+            });
+
+            it("output", [&] {
+                expect(dump(packed)).to(eq("8C,4D,B2,00,"));
+            });
         });
     });
-
-    // 101 001 100 = 1 0100 1100
-    describe("4 + 1 + 5", [] {
-
-        before("each", [] {
-            packed = p + 4 + 1 + 5;
-        });
-
-        it("size", [] {
-            expect(packed.size()).to(eq(2));
-        });
-
-        it("[1]", [] {
-            expect((uint16_t)packed[1]).to(eq(0x01));
-        });
-        it("all", [&] {
-            expect(dump(packed)).to(eq("4C,01,"));
-        });
-    });
-
-    describe("4 + 0 + 1 + 5", [&] {
-        before("each", [&] { packed = p + 4 + 0 + 1 + 5; });
-        it ("length", [&] { expect(packed.size()).to(eq(2)); });
-        it ("output", [&] { expect(dump(packed)).to(eq("44,0A,")); });
-    });
-
-    // 101 110 001 100 = 1011 1000 1100
-    describe("4 + 1 + 6 + 5", [&] {
-
-        before("each", [&] {
-            packed = p + 4 + 1 + 6 + 5;
-        });
-
-        it("size", [&] {
-            expect(packed.size()).to(eq(2));
-        });
-
-        it("[2]", [&] {
-            expect((uint16_t)packed[1]).to(eq(0xB));
-        });
-
-        it("all", [&] {
-            expect(dump(packed)).to(eq("8C,0B,"));
-        });
-    });
-
-    // 101 110 110 001 100 = 101 1101 1000 1100
-    describe("4 + 1 + 6 + 6 + 5", [&] {
-
-        before("each", [&] {
-            packed = p + 4 + 1 + 6 + 6 + 5;
-        });
-
-        it("all", [&] {
-            expect(dump(packed)).to(eq("8C,5D,"));
-        });
-    });
-
-    // 101 010 110 110 001 100 = 10 1010 1101 1000 1100
-    describe("4 + 1 + 6 + 6 + 2 + 5", [&] {
-
-        before("each", [&] { packed = p + 4 + 1 + 6 + 6 + 2 + 5; });
-
-        it("all", [&] { expect(dump(packed)).to(eq("8C,AD,02,")); });
-    });
-
-    // 0101 1001 0010 0110 110 001 100 = 0 1011 0010 0100 1101 1000 1100
-    describe("4, 1, 6, Shift(4) 6, 2, 9 (4 code bits)", [] {
-
-        before("each", [] {
-            packed = p + 4 + 1 + 6 + Shift(4) + 6  + 2 + 9 + 5;
-        });
-
-        it("all", [&] {
-            expect(dump(packed)).to(eq("8C,4D,B2,00,"));
-        });
-    });
-
 });
 
 } // namespace
