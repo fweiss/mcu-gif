e56a6a5b6cd281b0183bdbc4334fb20d56b22d12
Frank E Weiss <feweiss@gmail.com>
2020-07-12 16:29:21 -0700

    Postfactor: remove dead code

--- spec/file_read_spec.cc
+++ spec/file_read_spec.cc
@@ -9,24 +9,44 @@ using ccspec::expect;
 using ccspec::matchers::eq;
 using ccspec::matchers::be;
 
+using std::vector;
+
 #include "gd.h"
 #include "gd_internal.h"
 
 #include "helpers/fake_file.h"
 
+// a little DSL to concatenate vectors
+// provides for constructing test files out of blocks
+// doesn't have to be efficient
+vector<uint8_t> operator+(const vector<uint8_t> &a, const vector<uint8_t> &b) {
+    vector<uint8_t> r = a;
+    r.insert(r.end(), b.begin(), b.end());
+    return r;
+}
+
+static const vector<uint8_t> header({ 'G', 'I', 'F', '8', '9', 'a' });
+static const vector<uint8_t> logical_screen_descriptor({
+    0x0A, 0x00, // width
+    0x0A, 0x00, // height
+    0x81, // color map info
+    0x00, // background color index
+    0x00 // aspect ratio
+});
+static const vector<uint8_t> global_color_table({
+    0xff, 0xff, 0xff,
+    0xff, 0x00, 0x00,
+    0x00, 0x00, 0xff,
+    0x00, 0x00, 0x00
+});
+
 namespace simple {
 
 auto file_read_spec =
 describe("file read", [] {
-    static uint8_t header_logical_screen_descriptor[13] =
-    {
-        'G', 'I', 'F', '8', '9', 'a',
-        0x0A, 0x00, // width
-        0x0A, 0x00, // height
-        0x81, // color map info
-        0x00, // background color index
-        0x00 // aspect ratio
-    };
+//    test = header + logical_screen_descriptor;
+//    static uint8_t *header_logical_screen_descriptor = test.data();
+
     it("initial block type", [] {
         gd_main_t main;
         gd_init(&main);
@@ -34,9 +54,11 @@ describe("file read", [] {
         expect(type).to(eq(GD_BLOCK_INITIAL));
     });
     it("global color table type", [] {
+        vector<uint8_t> file = header + logical_screen_descriptor;
+        FFILE(file.data());
+
         gd_main_t main;
         gd_info_t info;
-        FFILE(header_logical_screen_descriptor);
         main.read = f_read;
 
         gd_init(&main);
@@ -44,6 +66,22 @@ describe("file read", [] {
         gd_block_type_t type = gd_next_block_type(&main);
 
         expect((int)type).to(eq((int)GD_BLOCK_GLOBAL_COLOR_TABLE));
+    });
+    it("graphic control extension type", [] {
+        vector<uint8_t> file = header + logical_screen_descriptor + global_color_table;
+        FFILE(file.data());
+
+        gd_main_t main;
+        gd_info_t info;
+        main.read = f_read;
+        uint8_t gct[4 * 3];
+
+        gd_init(&main);
+        gd_read_header(&main, &info);
+        gd_read_global_color_table(&main, gct);
+
+        gd_block_type_t type = gd_next_block_type(&main);
+        expect((int)type).to(eq((int)GD_BLOCK_GRAPHIC_CONTROL_EXTENSION));
     });
 });
 
--- src/gd.h
+++ src/gd.h
@@ -6,8 +6,9 @@
 typedef uint8_t gd_index_t;
 
 typedef enum {
-    GD_BLOCK_INITIAL,
-    GD_BLOCK_GLOBAL_COLOR_TABLE,
+    GD_BLOCK_INITIAL = 0,
+    GD_BLOCK_GLOBAL_COLOR_TABLE = 1,
+    GD_BLOCK_GRAPHIC_CONTROL_EXTENSION = 2,
 } gd_block_type_t;
 
 typedef struct {
@@ -33,6 +34,7 @@ void gd_open(gd_info_t *info);
 
 void gd_init(gd_main_t *main);
 gd_block_type_t gd_next_block_type(gd_main_t *main);
-void gd_read_header(gd_main_t *main, gd_info_t *info);
 
+void gd_read_header(gd_main_t *main, gd_info_t *info);
+void gd_read_global_color_table(gd_main_t *main, uint8_t *color_table);
 void gd_read_image(gd_main_t *main, gd_index_t *output, size_t capacity);
