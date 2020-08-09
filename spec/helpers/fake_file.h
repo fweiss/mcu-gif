50479c556e8f558bd29d0a28ac3bc47b29d9a7b3
Frank E Weiss <feweiss@gmail.com>
2020-08-09 14:23:31 -0700

    Add fake file with vector

--- spec/helpers/fake_file.h
+++ spec/helpers/fake_file.h
@@ -2,8 +2,11 @@
 
 #include <stdint.h>
 #include <stdio.h>
+#include <vector>
 
 extern void f_open_memory(uint8_t *data, long size);
 extern ssize_t f_read(int fd, void *buf, size_t count);
 
 #define FFILE(init) (f_open_memory(init, sizeof(init)))
+
+#define FFILEV(vec) (f_open_memory(vec.data(), vec.size()))
